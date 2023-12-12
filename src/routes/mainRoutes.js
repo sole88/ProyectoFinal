3e33fda4895361dc74762e8f9bc187345583b9cc
sole88 <castreccinisoledad@gmail.com>
2023-12-11 22:24:43 -0300

    configurando ruta /home a ruta / para vercel

--- src/routes/mainRoutes.js
+++ src/routes/mainRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const mainControllers = require ('../controllers/mainControllers')
 
-router.get('/home', mainControllers.home);
+router.get('/', mainControllers.home);
 router.get('/contact',mainControllers.contact);
 router.get('/about', mainControllers.about);
 router.get('/faqs', mainControllers.faqs);
